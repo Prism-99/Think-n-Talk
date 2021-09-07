457556ca9778e0ca9798d682b53ab662fd4f724e
Prism-99 <86083756+Prism-99@users.noreply.github.com>
2021-09-06 21:13:45 -0400

    Some house cleaning

--- Shared/SDV_BubbleGuy/SMAPI/ModEntry.cs
+++ Shared/SDV_BubbleGuy/SMAPI/ModEntry.cs
@@ -12,10 +12,6 @@ using HarmonyLib;
 #endif
 namespace SDV_Speaker.SMAPIInt
 {
-
-
-
-
     internal class ModEntry : Mod
     {
         private IModHelper oHelper;
@@ -23,28 +19,31 @@ namespace SDV_Speaker.SMAPIInt
         public override void Entry(IModHelper helper)
         {
             oHelper = helper;
-            oManager = new BubbleGuyManager(Path.Combine(helper.DirectoryPath, "saves"), Path.Combine(helper.DirectoryPath,"sprites") , helper,Monitor);
+            oManager = new BubbleGuyManager(Path.Combine(helper.DirectoryPath, "saves"), Path.Combine(helper.DirectoryPath, "sprites"), helper, Monitor);
             BubbleChat.Initialize(oManager);
             helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
 
 #if Current
                 Harmony harmony = new Harmony(ModManifest.UniqueID);
 #elif Classic
-            var harmony = HarmonyInstance.Create(ModManifest.UniqueID);
+            HarmonyInstance harmony = HarmonyInstance.Create(ModManifest.UniqueID);
 #endif
             harmony.Patch(
           original: AccessTools.Method(typeof(ChatBox), "runCommand", new Type[] { typeof(string) }),
           prefix: new HarmonyMethod(typeof(BubbleChat), nameof(BubbleChat.RunCommand))
           );
+
             Monitor.Log($"Harmony patch applied", LogLevel.Info);
 
         }
         private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
         {
-           
+
             BubbleGuyStatics.Initialize(Path.Combine(oHelper.DirectoryPath, "Sprites"));
+#if DEBUG
             Monitor.Log($"BubbleGuy name: '{BubbleGuyStatics.BubbleGuyName}'", LogLevel.Info);
+#endif
         }
     }
-   
+
 }
